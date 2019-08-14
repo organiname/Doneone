f652fe044e984ad2961ef27c76dbbdcbbaa30449
testliu <852889558@qq.com>
2019-08-14 11:22:06 +0800

    etwe

---
+++ src/liu/han/su/TestGit.java
@@ -0,0 +1,15 @@
+package liu.han.su;
+
+public class TestGit {
+	public static void main(String[] args) {
+		test01();
+		
+	}
+
+	private static void test01() {
+		System.out.println("12345");
+		
+	}
+	
+
+}
