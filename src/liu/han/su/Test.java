ac26b3dd799852f5726bb9da99850503a1c6f131
testliu <852889558@qq.com>
2019-08-14 10:00:49 +0800

    冲突合并

--- src/liu/han/su/Test.java
+++ src/liu/han/su/Test.java
@@ -2,8 +2,13 @@ package liu.han.su;
 
 public class Test {
 	public static void main(String[] args) {
+		syt();
+		
+	}
+
+	private static void syt() {
 		System.out.println("thired"+"liuhansu01");
-		System.out.println(Dog.name+"thired commit!!");
+		System.out.println(Cat.catName+"thired commit!!");
 		System.out.println(People.name+"first commit!!");
 	}
 
