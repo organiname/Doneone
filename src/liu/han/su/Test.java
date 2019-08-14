89135e9048206ee7348de69516832db16a0c562b
testliu <852889558@qq.com>
2019-08-14 10:05:40 +0800

    df

--- src/liu/han/su/Test.java
+++ src/liu/han/su/Test.java
@@ -3,6 +3,9 @@ package liu.han.su;
 public class Test {
 	public static void main(String[] args) {
 		syt();
+		System.out.println("thired"+"liuhansu01");
+		System.out.println(Cat.catName+"thired commit!!");
+		System.out.println(People.name+"first commit!!");
 		
 	}
 
