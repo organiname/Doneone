71f21bef16b677d375a457c70553a465082d3be9
testliu <852889558@qq.com>
2019-08-14 15:53:48 +0800

    dgfd

--- src/liu/han/su/TestGit.java
+++ src/liu/han/su/TestGit.java
@@ -3,12 +3,18 @@ package liu.han.su;
 public class TestGit {
 	public static void main(String[] args) {
 		test01();
+		test02();
+		
+	}
+
+	private static void test02() {
+	System.out.println("I'm ok");
 		
 	}
 
 	private static void test01() {
 
-	String s =	Sun.name;
+	String s =	Ye.name;
 
 		
 	}
